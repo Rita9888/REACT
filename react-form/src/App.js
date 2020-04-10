8f1541fb7a1809edcfefb8a13e7b5503b480b639
Margarita <melekskaya988@gmail.com>
2020-04-10 16:00:22 +0300

    without errorComponent

---
+++ react-form/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import RegisterForm from "./components/register-form";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Sing up</h1>
+      <RegisterForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-form/src/components/error-component.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./error-component";
+
+function ErrorComponent(validat) {
+  return (
+    <ul className="errorValid">
+      <li></li>
+    </ul>
+  );
+}
+
+export default ErrorComponent;
